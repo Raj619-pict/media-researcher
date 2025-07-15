2e061c55d4b824fceef52219d5d20e0c245ce0b2
Raj619-pict <rajclg13ho@gmail.com>
2025-07-16 03:48:03 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,10 +1,1 @@
-from pydantic import BaseModel
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    class Config:
-        orm_mode = True
-
-class CreatePost(PostBase):
-    pass
+ 
